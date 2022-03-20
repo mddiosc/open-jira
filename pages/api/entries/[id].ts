5b8ed774a1286ccd2cff4734eb17b212986fd7eb
Miguel Ángel de Dios <mddiosc@gmail.com>
2022-03-20 17:59:38 +0100

    update entry in back

---
+++ pages/api/entries/[id].ts
@@ -0,0 +1,69 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { db } from "../../../database";
+import { Entry, IEntry } from "../../../models";
+import mongoose from "mongoose";
+
+type Data = { message: string } | IEntry;
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (!mongoose.isValidObjectId(req.query.id)) {
+    return res.status(400).json({ message: "Invalid id" });
+  }
+
+  switch (req.method) {
+    case "GET":
+      return getOneEntry(req, res);
+
+    case "PUT":
+      return putEntry(req, res);
+
+    default:
+      return res.status(400).json({ message: "Method not allowed" });
+  }
+}
+
+const getOneEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  await db.connect();
+
+  try {
+    const entry = await Entry.findOne({ _id: req.query.id });
+    if (!entry) {
+      return res.status(404).json({ message: "Entry not found" });
+    }
+    return res.status(200).json(entry);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Algo salio mal, revisar consola del servidor" });
+  }
+};
+
+const putEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { id } = req.query;
+  await db.connect();
+  const entry = await Entry.findById(id);
+  if (!entry) {
+    await db.disconnect();
+    return res.status(404).json({ message: "Entry not found" });
+  }
+  const { description = entry.description, status = entry.status } = req.body;
+
+  try {
+    const updateEntry = await Entry.findByIdAndUpdate(
+      id,
+      { description, status },
+      { runValidators: true, new: true }
+    );
+    await db.disconnect();
+    return res.status(200).json(updateEntry!);
+  } catch (error) {
+    await db.disconnect();
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "Algo salio mal, revisar consola del servidor" });
+  }
+};
